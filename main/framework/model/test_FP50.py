124d5a3bb6d90b36fd59f168a392e75a8235c911
Sudhakar Fernando <sudhakar.c@emirates.com>
2019-03-13 19:14:53 +0400

    fp50 prepare step code converted from SAS file

--- main/framework/model/test_FP50.py
+++ main/framework/model/test_FP50.py
@@ -1,12 +1,12 @@
 from ModelPrepare import ModelPrepare
 import pickle
 # temp code to pickle results for easier development of FP50
-infile = open('FP50/test_FP50.pickle', 'rb')
-map_data_a, map_variables_v, input_vs, v_inverse_links=pickle.load(infile)
-infile.close()
-
+pickle_path = 'C:\\codes\\map_fp_50\\test_FP50_v2.pickle'
+with open(pickle_path, 'rb') as infile:
+    map_data_a,map_variables_v,input_vs,input_vs_logs,v_inverse_links,p_metrics,p_models,p_data,p_observations,p_dates=pickle.load(infile)
 # run prepare step
 mp = ModelPrepare()
-models_df, variable_details = mp.prepare_models(map_data_a, map_variables_v, input_vs, v_inverse_links)
+models_df,variable_details = mp.prepare_models(p_metrics,p_models,p_data,p_observations,p_dates)
 print('here')
 print(models_df)
+print(variable_details)
